b9515977d13206ac1ffa0534ec90d917c9a65299
Nickolay Sukonny <kajenick@gmail.com>
2020-03-20 10:57:21 +0300

    added full xPath support

--- src/jquery-get-xpath.js
+++ src/jquery-get-xpath.js
@@ -1,11 +1,10 @@
-(function( $ ) {
-    $.fn.jGetXpath = function() {
-        let path = '';
+(function ($) {
+    $.fn.jGetXpath = function (shortPath = false) {
 
         function checkId(that) {
 
             if (undefined !== that.attr('id')) {
-                if(1 === $('#' + that.attr('id')).length) {
+                if (isUniqueId(that.attr('id'))) {
                     let path = that.prop("tagName") + '#' + that.attr('id');
 
                     return path.toLowerCase();
@@ -17,10 +16,10 @@
 
         function checkClass(that) {
 
-            if (undefined !== that.attr('class') 
-                && undefined !== that[0] 
+            if (undefined !== that.attr('class')
+                && undefined !== that[0]
                 && 0 < that[0].classList.length) {
-                
+
                 for (let i = 0; i < that[0].classList.length; i++) {
                     if (isUniqueClass(that[0].classList[i])) {
                         let path = that.prop("tagName") + '.' + that[0].classList[i];
@@ -32,18 +31,52 @@
 
             return null;
         }
-        
+
+        function checkFullPath(that) {
+
+            let lastParent = that,
+                path = '';
+
+            while (0 !== lastParent.length) {
+                path = checkIndex(lastParent) + path;
+                lastParent = lastParent.parent();
+            }
+
+            return path.toLowerCase();
+        }
+
+        function checkIndex(elem) {
+
+            let tag = elem.prop("tagName");
+
+            if (undefined === tag) {
+
+                return '';
+            }
+
+            let neighbours = elem.parent().children(tag),
+                path = '/' + tag;
+
+            if (1 < neighbours.length) {
+                let index = neighbours.index(elem) + 1;
+
+                path = path + '[' + index + ']';
+            }
+
+            return path;
+        }
+
+        function isUniqueId(id) {
+
+            return 1 === $('#' + id).length;
+        }
+
         function isUniqueClass(className) {
 
             return 1 === $('.' + className).length;
         }
 
-        function checkFullPath(that) {
-
-            return null;
-        }
-
-        path = checkId(this);
+        let path = checkId(this);
 
         if (!path) {
             path = checkClass(this);
@@ -55,4 +88,14 @@
 
         return path;
     };
+
+    String.prototype.prepareForDomXPath = function () {
+
+        let domXPath = '';
+
+
+
+        return domXPath.toLowerCase();
+    };
+
 })(jQuery);
